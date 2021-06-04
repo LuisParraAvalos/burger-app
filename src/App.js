be8440674ac617e3353c3726cd9ff5ae3e7eba22
Luis Parra <luisparraavalos@hotmail.com>
2021-06-03 20:30:56 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import * as actions from './store/actions/index';
+import { connect } from 'react-redux';
+import asyncComponent from './hoc/asyncComponent';
 
-export default App;
+const asyncCheckout = asyncComponent(() => import('./containers/Checkout/Checkout'));
+const asyncOrders = asyncComponent(() => import('./containers/Orders/Orders'));
+const asyncLogout = asyncComponent(() => import('./containers/Auth/Logout/Logout'));
+const asyncAuth = asyncComponent(() => import('./containers/Auth/Auth'));
+
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.checkAuthState();
+  }
+
+  render() {
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={asyncAuth} />
+        <Route path='/' component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+    if (this.props.isAuth) {
+      routes = (
+        <Switch>
+          <Route path='/checkout' component={asyncCheckout} />
+          <Route path='/orders' component={asyncOrders} />
+          <Route path='/logout' component={asyncLogout} />
+          <Route path='/auth' component={asyncAuth} />
+          <Route path='/' component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
+    return (
+      <BrowserRouter>
+        <Layout>
+          {routes}
+        </Layout>
+      </BrowserRouter>
+    );
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
+  };
+};
+
+const mapDispatchersToProps = dispatch => {
+  return {
+    checkAuthState: () => dispatch(actions.checkAuthState())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchersToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducerIngredients from './store/reducers/ingredients';
+import reducerOrders from './store/reducers/orders';
+import reducerAuth from './store/reducers/auth';
+import thunk from 'redux-thunk';
+
+const logger = store => {
+  return next => {
+    return action => {
+      // console.log("[Middleware] - action: ", action);
+      const result = next(action);
+      // console.log("[Middleware] - new State: ", store.getState());
+      return result;
+    }
+  }
+}
+
+const rootReducer = combineReducers({
+  ings: reducerIngredients,
+  orders: reducerOrders,
+  auth: reducerAuth
+});
+
+// console.log("NODE_ENV: " + process.env.NODE_ENV);
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ?  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :  null || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
