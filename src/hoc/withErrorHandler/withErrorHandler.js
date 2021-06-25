4e96584ed699f42ff56c02c12cfa4c00d3b8656d
Luis Parra <luisparraavalos@hotmail.com>
2021-06-24 22:50:38 -0500

    Change HOC WithErrorHandler to Functional

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,46 +1,45 @@
 
-import React, { Component, useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Auxiliary from '../Auxiliary/Auxiliary';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-    return (class extends Component {
-        state = { error: null };
+    return ((props) => {
+        const [error, setError] = useState(null);
 
-        dismissModal = () => this.setState({ error: null });
+        const dismissModal = () => setError(null);
 
-        UNSAFE_componentWillMount() {
-            // console.log("Setup!");
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({ error: null });
-                return req;
-            }, error => {
-                this.setState({ error: error });
-                return Promise.reject(error);
-            });
+        const reqInterceptor = axios.interceptors.request.use(req => {
+            setError(null);
+            return req;
+        }, err => {
+            setError(err);
+            return Promise.reject(err);
+        });
 
-            this.resInterceptor = axios.interceptors.response.use(res => { return res; }, error => {
-                this.setState({ error: error });
-                return Promise.reject(error);
-            });
-        }
+        const resInterceptor = axios.interceptors.response.use(res => { return res; }, err => {
+            setError(err);
+            return Promise.reject(err);
+        });
 
-        componentWillUnmount() {
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.response.eject(this.resInterceptor);
-        }
+        useEffect(() => {
+            //Only clear interceptor on Cleanup options
+            return () => {
+                axios.interceptors.request.eject(reqInterceptor);
+                axios.interceptors.response.eject(resInterceptor);
+            };
+        }, [reqInterceptor, resInterceptor]);
 
-        render() {
-            // console.log("Error HOC: " + this.state.error);
-            return (
-                <Auxiliary>
-                    <Modal show={this.state.error} clicked={this.dismissModal} >
-                        {this.state.error ? this.state.error.message : null}
-                    </Modal>
-                    <WrappedComponent {...this.props} />
-                </Auxiliary>
-            );
-        }
+        
+        // console.log("Error HOC: " + state.error);
+        return (
+            <Auxiliary>
+                <Modal show={error} clicked={dismissModal} >
+                    {error ? error.message : null}
+                </Modal>
+                <WrappedComponent {...props} />
+            </Auxiliary>
+        );
     });
 };
 
